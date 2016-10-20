30bd5a08a003c20fad3a141b043ab8c7aa81463d
LimaLucas_ <loliveira@inf.ufsm.br>
2016-10-20 21:10:33 -0200

    Update
    - Alterado tab e 2 para 4 espaços;
    - Removida a pasta src, os aquivos foram colocados na pasta raiz;
    - Criado o arquivo index.php;
    - Criada a pasta Controller.

---
+++ apresentation/normalização.sql
@@ -0,0 +1,5 @@
+	baladaSegura_desnormalizada (data, municipio, endereco, CRVL_recolhida, CNH_recolhida, veiculo_recolhido, veiculo_autuado, recusou_teste_etiometro, autuado_teste_etiometro, qtd_autuacoes, tipo_veiculo, marca_modelo_veiculo)
+
+	1FN - Já está adequada.
+	2FN -
+		
---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<meta charset=utf-8>
+		<title>Trabalho PGBD</title>
+	</head>
+	<body>
+		<?php
+		/**
+		* Classe responsável por manter métodos de conexão com a base dados
+		* @author Anthony Tailer
+		* @author Lucas Lima
+		**/
+
+			function __autoload($classe){
+			  include_once "../model/{$classe}.class.php";
+			}
+
+		?>
+	</obdy>
+</html>
