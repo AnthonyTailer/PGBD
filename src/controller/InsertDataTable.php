dddcad0c9d4a227e08ef629d62400e4e7b8d31a1
anthonytailer <aalmeida@inf.ufsm.br>
2016-11-17 01:17:34 -0200

    Migracao.php e InsertDataTable.php atualizados

--- src/controller/InsertDataTable.php
+++ src/controller/InsertDataTable.php
@@ -44,6 +44,11 @@ if ($request == "desnormalizada") {
       $output, array($row["IDREGIAO"],$row["NOME"])
     );
   }
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+
 }elseif ($request == "estado") {
   $query = "SELECT E.IDESTADO, E.NOME, R.NOME as REGIAO FROM ESTADO E NATURAL JOIN REGIAO R LIMIT 1000";
   $result = $conex->executeQuery($query);
@@ -52,7 +57,125 @@ if ($request == "desnormalizada") {
       $output, array($row["IDESTADO"],$row["NOME"], $row["REGIAO"])
     );
   }
-}else{
-  
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+
+}elseif ($request == "cidade") {
+  $query = "SELECT C.NOME, E.NOME as ESTADO FROM CIDADE C NATURAL JOIN ESTADO E LIMIT 1000";
+  $result = $conex->executeQuery($query);
+  while($row = $result->fetch_array(MYSQLI_ASSOC)){
+    array_push(
+      $output, array($row["NOME"],$row["ESTADO"])
+    );
+  }
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+
+}else if ($request == "consumidor"){
+  $query = "SELECT C.SEXO, C.FAIXAETARIA, CI.NOME as NCIDADE FROM CONSUMIDOR C NATURAL JOIN CIDADE CI LIMIT 1000";
+  $result = $conex->executeQuery($query);
+  while($row = $result->fetch_array(MYSQLI_ASSOC)){
+    array_push(
+      $output, array($row["SEXO"],$row["FAIXAETARIA"], $row["NCIDADE"])
+    );
+  }
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+
+}else if ($request == "segmento"){
+  $query = "SELECT S.DESCRICAO, FROM SEGMENTO S LIMIT 1000";
+  $result = $conex->executeQuery($query);
+  while($row = $result->fetch_array(MYSQLI_ASSOC)){
+    array_push(
+      $output, array($row["SEGMENTO"])
+    );
+  }
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+
+}else if ($request == "area"){
+  $query = "SELECT A.DESCRICAO, S.DESCRICAO as SEGDESC FROM AREA A NATURAL JOIN SEGMENTO S LIMIT 1000";
+  $result = $conex->executeQuery($query);
+  while($row = $result->fetch_array(MYSQLI_ASSOC)){
+    array_push(
+      $output, array($row["DESCRICAO"],$row["SEGDESC"])
+    );
+  }
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+
+}else if ($request == "empresa"){
+  $query = "SELECT E.NOMEFANTASIA, A.DESCRICAO FROM EMPRESA E NATURAL JOIN AREA A LIMIT 1000";
+  $result = $conex->executeQuery($query);
+  while($row = $result->fetch_array(MYSQLI_ASSOC)){
+    array_push(
+      $output, array($row["NOMEFANTASIA"],$row["DESCRICAO"])
+    );
+  }
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+}else if ($request == "grupo" ){
+  $query = "SELECT G.DESCRICAO FROM GRUPO G LIMIT 1000";
+  $result = $conex->executeQuery($query);
+  while($row = $result->fetch_array(MYSQLI_ASSOC)){
+    array_push(
+      $output, array($row["DESCRICAO"])
+    );
+  }
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+
+}else if ($request == "problema"){
+  $query = "SELECT P.DESCRICAO, G.DESCRICAO AS GRUPODESC FROM PROBLEMA P NATURAL JOIN GRUPO G LIMIT 1000";
+  $result = $conex->executeQuery($query);
+  while($row = $result->fetch_array(MYSQLI_ASSOC)){
+    array_push(
+      $output, array($row["DESCRICAO"],$row["GRUPODESC"])
+    );
+  }
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+
+}else if ($request == "reclamacao"){
+  $query = "SELECT * FROM RECLAMACAO_DES LIMIT 1000";
+  $result = $conex->executeQuery($query);
+  while($row = $result->fetch_array(MYSQLI_ASSOC)){
+    array_push(
+      $output,
+      array(
+        $row["REGIAO"],$row["UF"],$row["CIDADE"],
+        $row["SEXO"],$row["FAIXAETARIA"],$row["ANOABERTURA"],
+        $row["MESABERTURA"],$row["DATAABERTURA"],$row["DATARESPOSTA"],
+        $row["DATAFINALIZACAO"], $row["TEMPORESPOSTA"],$row["NOMEFANTASIA"],
+        $row["SEGMENTOMERCADO"],$row["AREA"],$row["ASSUNTO"],
+        $row["GRUPOPROBLEMA"],$row["PROBLEMA"],$row["COMOCOMPROU"],
+        $row["PROCUROUEMPRESA"],$row["RESPONDIDA"],$row["SITUACAO"],
+        $row["AVALIACAO"],$row["NOTACONSUMIDOR"]
+      )
+    );
+  }
+
+  echo "{
+          \"data\":".json_encode($output)."
+        }";
+
+}else {
+  echo "Error";
 }
 ?>
--- src/controller/Migracao.php
+++ src/controller/Migracao.php
@@ -10,35 +10,145 @@ $desnormalizada = new MagicDesnormalizada();
 $DAODesnormalizada = new DAODesnormalizada();
 
 $request = $_GET['tabela'];
+$porcentagem = 10;
 
-$queryRegiao;
 
 if ($request == "regiao") {
 
+  $query = "INSERT INTO REGIAO (NOME)
+  SELECT DISTINCT REGIAO FROM RECLAMACAO_DES ORDER BY ASC" ;
 
-  $result = $conex->executeQuery($queryRegiao);
-  while($row = $result->fetch_array(MYSQLI_ASSOC)){
-
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
   }
-
 
 }else if ($request == "estado"){
 
+  $query = "INSERT INTO ESTADO (NOME, IDREGIAO)
+    SELECT DISTINCT UF, IDREGIAO FROM RECLAMACAO_DES RD
+    JOIN REGIAO R ON RD.REGIAO = R.NOME" ;
+
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
+  }
+
 }else if ($request == "cidade"){
+
+  $query = "INSERT INTO CIDADE (NOME, IDESTADO)
+    SELECT DISTINCT CIDADE, IDESTADO FROM RECLAMACAO_DES RD
+    JOIN ESTADO E ON RD.ESTADO = E.NOME";
+
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
+  }
 
 }else if ($request == "consumidor"){
 
+  $query = "INSERT INTO CONSUMIDOR (SEXO, FAIXAETARIA, IDCIDADE)
+    SELECT DISTINCT SEXO, FAIXAETARIA, IDCIDADE FROM RECLAMACAO_DES RD
+    JOIN CIDADE C ON RD.CIDADE = C.NOME";
+
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
+  }
+
 }else if ($request == "segmento"){
+
+  $query = "INSERT INTO SEGMENTO (DESCRICAO) SELECT DISTINCT
+  SEGMENTOMERCADO FROM RECLAMACAO_DES ORDER BY ASC
+";
+
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
+  }
 
 }else if ($request == "area"){
 
+  $query = "INSERT INTO AREA (DESCRICAO, IDSEGMENTO)
+    SELECT DISTINCT AREA, IDSEGMENTO FROM RECLAMACAO_DES RD
+    JOIN SEGMENTO S ON RD.SEGMENTOMERCADO = S.DESCRICAO";
+
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
+  }
+
 }else if ($request == "empresa"){
+
+  $query = "INSERT INTO EMPRESA (NOMEFATASIA, IDAREA)
+    SELECT DISTINCT NOMEFATASIA, IDAREA FROM RECLAMACAO_DES RD JOIN
+     AREA A ON RD.AREA = A.DESCRICAO";
+
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
+  }
 
 }else if ($request == "grupo"){
 
+  $query = "INSERT INTO GRUPO (DESCRICAO) SELECT DISTINCT
+   GRUPOPROBLEMA FROM RECLAMACAO_DES RD";
+
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
+  }
+
 }else if ($request == "problema"){
 
+  $query = "INSERT INTO PROBLEMA (DESCRICAO, IDGRUPO)
+    SELECT DISTINCT PROBLEMA, IDGRUPO FROM RECLAMACAO_DES RD
+    JOIN GRUPO G ON RD.GRUPOPROBLEMA = G.DESCRICAO";
+
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
+  }
+
 }else if ($request == "reclamacao"){
+
+  $query =
+  "INSERT INTO RECLAMACAO (IDCONSUMIDOR, ANO, MES, DATAABERTURA, DATARESPOSTA,
+     DATAFINALIZACAO, TEMPORESPOSTA, IDEMPRESA, ASSUNTO, IDPROBLEMA, COMOCOMPROU,
+      PROCUROUEMPRESA, RESPONDIDA, SITUACAO, AVALIACAO, NOTACONSUMIDOR)
+      SELECT IDCONSUMIDOR, ANO, MES, DATAABERTURA, DATARESPOSTA, DATAFINALIZACAO,
+      TEMPORESPOSTA, IDEMPRESA, ASSUNTO, IDPROBLEMA, COMOCOMPROU, PROCUROUEMPRESA,
+       RESPONDIDA, SITUACAO, AVALIACAO, NOTACONSUMIDOR FROM RECLAMACAO_DES RD
+          JOIN CIDADE C ON RD.CIDADE = C.DESCRICAO
+          JOIN CONSUMIDOR CO ON RD.SEXO = CO.SEXO AND RD.FAIXAETARIA = CO.FAIXAETARIA
+          JOIN EMPRESA E ON RD.NOMEFATASIA = E.NOMEFATASIA
+          JOIN PROBLEMA P ON RD.PROBLEMA = P.DESCRICAO
+  ";
+
+  try {
+    $result = $conex->executeQuery($query);
+    echo $porcentagem;
+  } catch (Exception $e) {
+    echo 0;
+  }
 
 }else{
   echo "Error";
