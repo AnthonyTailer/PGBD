b4c12ae1777f7a95a4ddcbe8fc1db0036b08c983
anthonytailer <aalmeida@inf.ufsm.br>
2016-11-28 19:56:33 -0200

    fineshed commit.sh

--- commit.sh
+++ commit.sh
@@ -8,12 +8,12 @@ echo -e "\n|----> Do you want COMMIT your changes?(YES/NO or Y/N), followed by [
 
 read answer
 
-if (( (( "$answer" == "y" )) || (( "$answer" == "yes" )) )); then
+if [ "${answer,,}" == "y" ] || [ "${answer,,}" == "yes" ]; then
 	git pull
 	git add *
 	git commit -a -m "$str"
 	git push origin master
-elif (( (( "$answer" == "n" )) || (( "$answer" == "no" )) )); then
+elif [ "${answer,,}" == "n" ] || [ "${answer,,}" == "no" ]; then
 	echo -e "\n|----> Nothing was COMMITED!!."
 else
 	echo -e "\n|----> Invalid Input!!."
