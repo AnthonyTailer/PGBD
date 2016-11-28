8372d8e36348ffc6a601d9f0421a00fe20a32039
anthonytailer <aalmeida@inf.ufsm.br>
2016-11-28 18:51:18 -0200

    update commit.sh

--- commit.sh
+++ commit.sh
@@ -3,6 +3,16 @@
 str="$*"
 
 git status
-git add *
-git commit -a -m "$str"
-git push origin master
+
+echo -e "\n|----> Do you want COMMIT your changes?(YES/NO or Y/N), followed by [ENTER]:"
+
+read answer
+
+if (( (( "$answer" == "y" )) || (( "$answer" == "yes" )) )); then
+	git add *
+	git commit -a -m "$str"
+	git push origin master
+else
+	echo "Nothing was COMIITED!!."
+fi
+
